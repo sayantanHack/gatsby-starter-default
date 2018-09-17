92c8708a1d3a3c09f59f9fcd3ea6883c70429b11
sayantanHack <sayantandatta97@gmail.com>
2018-09-17 12:01:43 +0530

    added main files

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi Everyone this is Sayantan</h1>
+
+    <p>Welcome to my new Gatsby site.</p>
+    <p>Hi I am Sayantan , tech & security enthusiasht . I love Opensource & wanna contribute as much as I can .I love to code also. </p>
     <Link to="/page-2/">Go to page 2</Link>
+    <p>for more information please visit <a href="http://sayantan.cf">This site</a></p>
   </div>
 )
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,6 +6,7 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    
   </div>
 )
 
